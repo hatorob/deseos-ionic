ab0bc108d5fcbcd0eedd5311fed8a26b266da37b
Hector Toro <alejotoro94@hotmail.com>
2022-01-30 21:35:11 -0500

    Componente de Pendientes, estructura y Tabs

--- src/app/pages/explore-container/explore-container.module.ts
+++ src/app/pages/explore-container/explore-container.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
 import { ExploreContainerComponent } from './explore-container.component';
+
 
 @NgModule({
   imports: [ CommonModule, FormsModule, IonicModule],
