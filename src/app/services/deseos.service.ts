e7f420aa7a849f13a8a0f670fcfb7556601a1cec
Hector Toro <alejotoro94@hotmail.com>
2022-01-31 17:32:32 -0500

    LocalStorage - Hacer persistente la información

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -11,10 +11,12 @@ export class DeseosService {
 
   constructor() { 
 
-    const lista1 = new Lista('Recolectar piedras del infinito');
-    const lista2 = new Lista('Heroes a desaparecer');
+    this.cargarStorage();
 
-    this.listas.push(lista1,lista2);
+    /* const lista1 = new Lista('Recolectar piedras del infinito');
+    const lista2 = new Lista('Heroes a desaparecer'); */
+
+    /* this.listas.push(lista1,lista2); */
     
 
   }
@@ -22,6 +24,17 @@ export class DeseosService {
   crearLista( titulo: string) {
     const nuevaLista = new Lista(titulo);
     this.listas.push(nuevaLista);
-    
+    this.guardarStorage();
+  }
+
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+  cargarStorage() {
+    if( localStorage.getItem('data') ){
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    } else {
+      this.listas = [];
+    }
   }
 }
