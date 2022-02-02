07ea03bbab6a339a915187271fd52b502d30973e
Hector Toro <alejotoro94@hotmail.com>
2022-02-01 23:27:19 -0500

    Componente listas

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Lista } from 'src/app/models/lista.model';
+import { DeseosService } from 'src/app/services/deseos.service';
 
 @Component({
   selector: 'app-listas',
@@ -7,8 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListasComponent implements OnInit {
 
-  constructor() { }
+  @Input() terminada = true;
+
+  constructor( public deseosService: DeseosService,
+                private router: Router) { }
 
   ngOnInit() {}
+  
+  listaSeleccionada( lista: Lista ) {
 
+    console.log(lista);
+
+    if( this.terminada ) {
+      this.router.navigateByUrl(`tabs/tab2/agregar/${ lista.id }`);
+    } else {
+      this.router.navigateByUrl(`tabs/tab1/agregar/${ lista.id }`);
+    }
+  }
 }
