d6a2a4004380b2cfc5d252fab2fe527a59fbf211
Hector Toro <alejotoro94@hotmail.com>
2022-02-06 18:39:35 -0500

    * Editar el titulo de la lista, se termina la APP

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { AlertController, IonList } from '@ionic/angular';
 import { Lista } from 'src/app/models/lista.model';
 import { DeseosService } from 'src/app/services/deseos.service';
 
@@ -11,10 +11,13 @@ import { DeseosService } from 'src/app/services/deseos.service';
 })
 export class ListasComponent implements OnInit {
 
+  @ViewChild( IonList ) lista: IonList;
+
   @Input() terminada = true;
 
   constructor( public deseosService: DeseosService,
-                private router: Router) { }
+                private router: Router,
+                private alertCtrl: AlertController) { }
 
   ngOnInit() {}
   
@@ -31,6 +34,46 @@ export class ListasComponent implements OnInit {
 
   borrarLista( lista: Lista ) {
     this.deseosService.borrarLista( lista );
+  }
+
+  async editarNombre( lista: Lista ) {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Editar lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          value: lista.titulo,
+          placeholder: 'nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log("Cancelar");
+            this.lista.closeSlidingItems();
+          }
+        },
+        {
+          text: 'Actualizar',
+          handler: ( data ) => {
+            console.log(data);
+            if( data.titulo.length === 0 ) {
+              return;
+            }
+            // Aquí creamos la lista
+            lista.titulo = data.titulo;
+            this.deseosService.guardarStorage();
+            this.lista.closeSlidingItems();
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
   
 }
